31c4f92716ed420d06e03499100830e905cdc816
Katsutoshii <Josiah.K.Putman.20@Dartmouth.edu>
2017-10-31 13:05:19 -0400

    Created project

---
+++ app/src/main/java/com/wabalub/cs65/weplaylist/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wabalub.cs65.weplaylist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
