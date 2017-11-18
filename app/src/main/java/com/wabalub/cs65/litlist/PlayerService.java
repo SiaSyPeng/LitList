65c08cfb17eb6c0f9204bf0a80330a036fdda92e
Josiah Putman <Josiah.K.Putman.20@Dartmouth.edu>
2017-11-18 18:35:03 -0500

    Fixed notification

--- app/src/main/java/com/wabalub/cs65/litlist/PlayerService.java
+++ app/src/main/java/com/wabalub/cs65/litlist/PlayerService.java
@@ -8,17 +8,25 @@ import android.app.Service;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Icon;
+import android.media.session.MediaSession;
 import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.google.common.base.Joiner;
 import com.spotify.sdk.android.player.SpotifyPlayer;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import kaaes.spotify.webapi.android.models.ArtistSimple;
+import kaaes.spotify.webapi.android.models.Image;
 import kaaes.spotify.webapi.android.models.Track;
 
 public class PlayerService extends Service {
@@ -54,38 +62,75 @@ public class PlayerService extends Service {
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     private void updateNotification(){
         Log.d(TAG, "Updating notification");
-        if(currentTrack == null) return;
+        Notification.Builder builder;
 
-        // setup a pending intent so if the user clicks on the notification it can open the main activity
-        PendingIntent resultPendingIntent = getPendingIntent();
+        // if we don't have a song, notify with the default notification
+        if(currentTrack == null) {
+            // build the notification
+            builder = new Notification.Builder(this)
+                    .setContentTitle("LitList Audio Player")
+                    .setContentText("Songs will be displayed here")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setStyle(new Notification.MediaStyle())
+                    .setAutoCancel(false)
+                    .setOngoing(true)
+                    .setShowWhen(true);
+            return;
+        }
 
-        // build the notification
-        Notification.Builder builder = new Notification.Builder(this)
-                        .setSmallIcon(R.drawable.icon)
-                        .setContentTitle("Playing " + currentTrack.name)
-                        .setContentText("Artist " + namesToString(currentTrack.artists))
-                        .setAutoCancel(false)
-                        .setOngoing(true)
-                        .setShowWhen(true)
-                        .setContentIntent(resultPendingIntent);
+        // otherwise show the song that's playing
+        else {
+
+            // setup a pending intent so if the user clicks on the notification it can open the main activity
+            PendingIntent resultPendingIntent = getPendingIntent();
+
+            //get the album art
+            Image album_art = currentTrack.album.images.get(0);
+            Bitmap icon = null;
+            try {
+                icon = BitmapFactory.decodeStream((InputStream) new URL(album_art.url).getContent());
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+
+            // build the notification
+            builder = new Notification.Builder(this)
+                    .setContentTitle("Track: " + currentTrack.name)
+                    .setContentText("Artist: " + namesToString(currentTrack.artists))
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setLargeIcon(icon)
+                    .setStyle(new Notification.MediaStyle())
+                    .setAutoCancel(false)
+                    .setOngoing(true)
+                    .setShowWhen(true)
+                    .setContentIntent(resultPendingIntent);
+        }
 
         Notification notification = builder.build();
+        //startForeground(SERVICE_ID, notification);
+
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         if (notificationManager != null) {
+            Log.d(TAG, "Notifying!");
             notificationManager.notify(NOTIFICATION_ID, notification);
+        }
+        else {
+            Log.e(TAG,"Notification manager is null!");
         }
     }
 
     @Override
     public void onDestroy() {
         previewPlayer.release();
+        //player.shutdown();
         super.onDestroy();
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private PendingIntent getPendingIntent(){
         Intent resultIntent = new Intent(this, MainActivity.class);
+        resultIntent.putExtra(MainActivity.EXTRA_TOKEN, MainActivity.token);
 
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
 
